5524c1bf15fcf7b415dc050bca6e712019530a9f
Kamran Abdullah <kamranabdullah97@gmail.com>
2020-09-05 13:19:59 -0500

    moved files to different directory

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = 'pk.eyJ1Ijoia2FtcmFuNDIiLCJhIjoiY2tlYWs5a2QyMDFkdDMwczExbHFwY2ltMyJ9.m3ImXKRTFmjgWYBBv3Z50A';
---
+++ Leaflet-Step-1/static/js/level1.js
@@ -0,0 +1,57 @@
+// code for creating Basic Map (Level 1)
+// Creating map object
+const myMap = L.map("map", {
+    center: [40.7, -94.5],
+    zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// create function to determine color based on significance of eq
+function getStyle(sig){
+    let color;
+    if (sig <= 250) {
+        return '#fee5d9';
+    }
+    else if (sig <= 500) {
+        return '#fcae91';
+    }
+    else if (sig <= 750){
+        return '#fb6a4a';
+    }
+    else if (sig <= 1000){
+        return '#de2d26';
+    }
+    else {
+        return '#a50f15';
+    };
+}
+
+
+// Store API query variables
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson';
+
+// Grab the data with d3 and reason geojson
+d3.json(url).then( 
+    jsonData => {
+        console.log(jsonData.features);
+        jsonData.features.forEach(response => {
+            L.circle([response.geometry.coordinates[1], response.geometry.coordinates[0]],
+                {
+                    fillOpacity: .65,
+                    // color: getStyle(response.properties.sig),
+                    fillColor: getStyle(response.properties.sig),
+                    radius: response.properties.mag **3*1000,
+                    stroke:false
+                }
+        ).addTo(myMap);
+    })
+});
